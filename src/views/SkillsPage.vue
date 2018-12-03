41fef9e24b18362a056a1072a02d919edacc0af0
Hyperreal <hyperreal@Hyperreals-Mini.home>
2018-12-02 21:31:29 -0500

    skills height removed

--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -34,7 +34,7 @@ export default {
 <style lang="scss" scoped>
 .skills-wrap {
   width: 100vw;
-  height: -webkit-fill-available;
+  // height: -webkit-fill-available;
   text-align: center;
   transition: all 0.5s;
   .skills {
